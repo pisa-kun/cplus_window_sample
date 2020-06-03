23b5c4ae334ca8f73f7086b0b2cc84a5019f6f37
pisa-kun <pisa0401mst@gmail.com>
2020-06-03 19:47:04 +0900

    ctrl+イベント

--- timer_and_keyevent/timer_and_keyevent.cpp
+++ timer_and_keyevent/timer_and_keyevent.cpp
@@ -4,16 +4,27 @@
 #include "framework.h"
 #include "timer_and_keyevent.h"
 #include <iostream>
+#include "resource.h"
 
 #define MAX_LOADSTRING 100
 #define TM_COUNT1 1
 #define Seconds 1000
 #define BUTTON_ID1 99
+#define BUTTON_ID2 98
+#define BUTTON_ID3 97
 
 // グローバル変数:
 HINSTANCE hInst;                                // 現在のインターフェイス
 WCHAR szTitle[MAX_LOADSTRING];                  // タイトル バーのテキスト
 WCHAR szWindowClass[MAX_LOADSTRING];            // メイン ウィンドウ クラス名
+
+LPCTSTR strItem[] = {
+    TEXT("60  (1min)") ,
+    TEXT("180 (3min)") ,
+    TEXT("300 (5min)") ,
+};
+
+int lastTime[] = { 60,180,300 }; // strItemのstrをint変換する配列
 
 // このコード モジュールに含まれる関数の宣言を転送します:
 ATOM                MyRegisterClass(HINSTANCE hInstance);
@@ -108,8 +119,24 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
    CreateWindow(
        TEXT("BUTTON"), TEXT("Hoge"),
        WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
-       0, 400, 100, 50,
+       0, 200, 100, 50,
        hWnd, (HMENU)BUTTON_ID1, hInstance, NULL
+   );
+
+   // クリックされたらメッセージボックス用のwindowを作成する
+   CreateWindow(
+       TEXT("BUTTON"), TEXT("CreateWindow"),
+       WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
+       400, 200, 100, 50,
+       hWnd, (HMENU)BUTTON_ID2, hInstance, NULL
+   );
+
+   // クリックされたらメッセージボックス用のwindowを作成する
+   CreateWindow(
+       TEXT("BUTTON"), TEXT("Cange"),
+       WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
+       300, 200, 100, 50,
+       hWnd, (HMENU)BUTTON_ID3, hInstance, NULL
    );
 
    if (!hWnd)
@@ -139,36 +166,91 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wp, LPARAM lp) {
     RECT rctSize;
     static int iCount1 = 60; // 60秒タイマー
     static TCHAR strCount[64];
+    static TCHAR strText[64];
 
-    switch (msg) {
+    static HWND combo, label;
+    int i;
+
+    switch (msg) 
+    {
     case WM_DESTROY:
         PostQuitMessage(0);
         return 0;
     case WM_CREATE:
         SetTimer(hwnd, TM_COUNT1, Seconds, NULL); // 1000ミリ(1second)毎に変化するタイマー
+
+        // コンボボックスの作成
+        // CBS_DROPDOWNLIST で入力不可のリスト表示
+        combo = CreateWindow(
+            TEXT("COMBOBOX"), NULL,
+            WS_CHILD | WS_VISIBLE |  CBS_DROPDOWNLIST,
+            400, 0, 200, 200, hwnd, (HMENU)1,
+            ((LPCREATESTRUCT)(lp))->hInstance, NULL);
+        // コンボボックスに要素を追加
+        for (i = 0; i < 3; i++)
+            SendMessage(combo, CB_ADDSTRING, 0, (LPARAM)strItem[i]);
+
+        // コンボボックスの内容を表示するラベルを作成する
+        label = CreateWindow(
+            TEXT("STATIC"), NULL,
+            WS_CHILD | WS_VISIBLE,
+            0, 100, 200, 100, hwnd, (HMENU)2,
+            ((LPCREATESTRUCT)(lp))->hInstance, NULL);
+
         return 0;
     case WM_COMMAND:
-        switch (LOWORD(wp)) {
-        case BUTTON_ID1:
-            iCount1 = 444;
-            break;
+        if (HIWORD(wp) == CBN_SELCHANGE)
+        {
+            auto time = lastTime[SendMessage(combo, CB_GETCURSEL, 0, 0)];
+            wsprintf(strText, TEXT("アイテム数 = %d\n選択項目 = %d\n待機時間 = %d"),
+                SendMessage(combo, CB_GETCOUNT, 0, 0),
+                SendMessage(combo, CB_GETCURSEL, 0, 0),
+                time);
+            SetWindowText(label, strText);
+            // iCountをSetされた時間に更新する
+            // 忘れずに再描画
+            iCount1 = time;
+            InvalidateRect(hwnd, NULL, TRUE);
         }
-    case WM_KEYDOWN:
-        switch (wp) {
-        case VK_CONTROL:
-            iCount1 = 222;
-            break;
-        case VK_RIGHT:
-            iCount1 = 333;
-            break;
-        case VK_MENU: //ctrl+alt = alt扱い？
-            
-                // ctrl + alt、でウインドウ表示。yesだとカウンターの数値を2倍にする
-                auto result = MessageBox(hwnd, L"push", L"title", MB_OKCANCEL);
-                if (result == 1 /*MB_OK = yes*/) {
-                    iCount1 = 2 * Seconds;
-                }
+
+        if (LOWORD(wp) == BUTTON_ID1)
+        {
+            bool isCtrlPress = (GetKeyState(VK_CONTROL) < 0);
+            bool isAltPress = (GetKeyState(VK_MENU) < 0);
+
+            if (isCtrlPress && isAltPress) 
+            {
+                iCount1 = 2 * Seconds;
+                InvalidateRect(hwnd, NULL, TRUE);
+                break;
+            }
         }
+
+        // create ボタン
+        if (LOWORD(wp) == BUTTON_ID2)
+        {
+            // 第一引数に親ウインドウを指定できる
+            HWND hWnd2 = CreateWindowW(TEXT("STATIC"), L"Hoge Utility", WS_OVERLAPPEDWINDOW | WS_VISIBLE,
+                CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, hwnd, NULL, hInst, NULL);
+
+            CreateWindow(
+                TEXT("BUTTON"), L"シャットダウン",
+                WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
+                0, 0, 100, 50,
+                hWnd2, NULL, hInst, NULL
+            );
+        }
+
+        // ボタンのラベルを変更する
+        if (LOWORD(wp) == BUTTON_ID3)
+        {
+            // リソースファイルから文字を取得
+            // 104に格納している文字
+            TCHAR str1[64];
+            LoadString(hInst, IDS_STRING104, str1, sizeof(str1));
+            SetDlgItemText(hwnd, BUTTON_ID2, str1);
+        }
+
     case WM_TIMER:
         switch (wp) {
         case TM_COUNT1:
